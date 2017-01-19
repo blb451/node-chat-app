f307b0b8de353fc1166eca768b4cd31063bd2ff6
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-01-18 17:19:50 -0800

    9.117

--- public/js/index.js
+++ public/js/index.js
@@ -9,17 +9,17 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
+  var formattedTime = moment(message.createdAt).format('h:mm a')
   var li = $('<li></li>');
-  li.text(`${message.from}: ${message.text}`)
+  li.text(`[${formattedTime}] ${message.from}: ${message.text}`)
   $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
-  console.log('newLocationMessag', message);
+  var formattedTime = moment(message.createdAt).format('h:mm a')
   var li = $('<li></li>');
   var a = $('<a target="_blank">My Current Location</a>');
-  li.text(`${message.from}: `);
+  li.text(`[${formattedTime}] ${message.from}: `);
   a.attr('href', message.url);
   li.append(a);
   $('#messages').append(li);
