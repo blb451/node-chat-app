8f67e480875fdba33a5488dc1f90b4ef6a7a150c
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-01-18 16:14:59 -0800

    9.113

--- public/js/index.js
+++ public/js/index.js
@@ -24,3 +24,18 @@ $('#message-form').on('submit', function (event) {
     console.log('Message succesful');
   });
 });
+
+var locationButton = $('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.')
+  }
+  navigator.geolocation.getCurrentPosition(function (position) {
+     socket.emit('createLocationMessage', {
+       latitude: position.coords.latitude,
+       longitude: position.coords.longitude
+     });
+  }, function () {
+    alert('Unable to fetch location.')
+  });
+});
